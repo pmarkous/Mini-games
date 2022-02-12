dceebf4aab48445e5b85026563477bc10ba5a0eb
pmarkous <38925774+pmarkous@users.noreply.github.com>
2022-02-12 20:01:48 -0300

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,8 @@
+adj = input("Adjetive: ")
+verb1 = input("Verb: ")
+verb2= input("Verb: ")
+famous_person = input("Famous person: ")
+madlib = f"computerprogrammin si so {adj}! It makes me so excited all the tiem because \
+I love to {verb1}. Stay hydarated and {verb2} like you are {famous_person}"
+
+print(madlib)
